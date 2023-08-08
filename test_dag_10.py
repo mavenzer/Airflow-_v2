a073bb9b5646d2cff8dba8ece93c0f5e4fc7e9c7
Soumen <soumen1696@gmail.com>
2023-08-08 15:33:19 +0200

    Create test_dag_10.py

---
+++ test_dag_10.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+def print_task_number(task_number: int):
+    print(f"Executing task number {task_number}")
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 8, 8),
+    'retries': 1,
+}
+
+with DAG(
+    'parallel_execution_dag',
+    default_args=default_args,
+    description='A simple DAG to test parallel execution',
+    schedule_interval='@once',
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    # Create 10 tasks that will run in parallel
+    for i in range(10):
+        task = PythonOperator(
+            task_id=f'task_number_{i}',
+            python_callable=print_task_number,
+            op_args=[i],
+        )
